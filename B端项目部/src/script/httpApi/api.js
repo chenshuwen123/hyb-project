12e016207986f27ed15399b0d1e68ea5dad0d857
chenshuwen <1364115877@qq.com>
2017-04-11 17:00:20 +0800

    Update api.js

--- B端项目部/src/script/httpApi/api.js
+++ B端项目部/src/script/httpApi/api.js
@@ -5,7 +5,6 @@ angular.module('app')
         	this.api=function() {
         		//公共接口
         		var api='http://59.110.164.152';
-        	/*var api='http://10.130.31.56:8081';*/
         		return  api;
         	}
         	
